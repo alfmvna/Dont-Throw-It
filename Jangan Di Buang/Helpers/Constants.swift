c40dc8c03b41a8de5ff51d022c2d29adae119b47
Allif Maulana <alfmvna@gmail.com>
2020-03-08 21:00:31 +0700

    update constant and utilitites

---
+++ Jangan Di Buang/Helpers/Constants.swift
@@ -0,0 +1,27 @@
+///Users/alfmuna/Downloads/CustomLoginDemo 2/CustomLoginDemo/Helpers/Constants.swift
+//  Constants.swift
+//  CustomLoginDemo
+//
+//  Created by Christopher Ching on 2019-07-23.
+//  Copyright © 2019 Christopher Ching. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+        static let awalViewController = "Awal"
+        
+        static let myTabbar           = "myTabbar"
+        
+        static let SignInViewController = "SignInViewController"
+        
+        static let VC = "VC"
+    }
+    
+    
+}
